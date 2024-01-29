7a0066ec768b5792f9b647ccc9e40ae360131250
francisco1423mkt <franciscomaitre@outlook.com>
2024-01-29 10:31:48 -0300

    finalizei aula 015
    operadores Math

---
+++ index.js
@@ -0,0 +1,13 @@
+let num1 = 25;
+//let num2 = Math.floor(num1); Arredonda para baixo.
+//let num2 = Math.ceil(num1); Arredonda para cima.
+//let num2 = Math.round(num1); Arredonda para o mais proximo.
+//console.log(Math.max(1,2,300,400,18904));  Retorna o maior numero. para o menor use(Math.min)
+//console.log (Math.random()) // Gera um numero aleatório.
+//console.log(num2);
+//const aleatório = Math.round( Math.random() * (20 -0) + 0);
+//console.log(aleatório);
+//console.log(Math.pow(2,10))// potenciação.
+//console.log(2**10)
+//console.log(num1 ** 0.5); // raiz2.
+//console.log(100/02);//divisão.
